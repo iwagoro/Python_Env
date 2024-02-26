8139a271c9c6651d176e9d0da453a9cd742ca91c
rockwell <iwagoro733@gmail.com>
2024-02-27 00:06:16 +0900

    first commit

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mamba init -q           # mambaを初期化
+mamba update python -qy # Pythonを最新にアップデート
+mamba install ruff -qy  # Ruffをインストール
+mamba update -qy --all  # 全パッケージのアップデート
+mamba clean -qafy       # キャッシュ等を削除
+mamba  install --yes --file requirements.txt
